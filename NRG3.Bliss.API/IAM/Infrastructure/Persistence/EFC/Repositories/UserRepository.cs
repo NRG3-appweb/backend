af2d365101a46152e166c66ca7182ce46463ffea
NeoRise456 <neorise456@gmail.com>
2024-11-16 03:04:15 -0500

    refactor: moved user repository impl to IAM context.

--- NRG3.Bliss.API/IAM/Infrastructure/Persistence/EFC/Repositories/UserRepository.cs
+++ NRG3.Bliss.API/IAM/Infrastructure/Persistence/EFC/Repositories/UserRepository.cs
@@ -1,6 +1,6 @@
 ﻿
-using NRG3.Bliss.API.AppointmentManagement.Domain.Repositories;
 using NRG3.Bliss.API.IAM.Domain.Model.Aggregate;
+using NRG3.Bliss.API.IAM.Domain.Repositories;
 using NRG3.Bliss.API.Shared.Infrastructure.Persistence.EFC.Configuration;
 using NRG3.Bliss.API.Shared.Infrastructure.Persistence.EFC.Repositories;
 
