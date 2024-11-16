6feba9b94b46fff4a408c8005f1b18ff9f67f663
NeoRise456 <neorise456@gmail.com>
2024-11-16 03:03:39 -0500

    feat(service-management): add context facade for service.

---
+++ NRG3.Bliss.API/ServiceManagement/Interfaces/ACL/IServiceContextFacade.cs
@@ -0,0 +1,8 @@
+﻿using NRG3.Bliss.API.ServiceManagement.Domain.Model.Aggregates;
+
+namespace NRG3.Bliss.API.ServiceManagement.Interfaces.ACL;
+
+public interface IServiceContextFacade
+{
+    Task<Service> FetchServiceByIdAsync(int serviceId);
+}
