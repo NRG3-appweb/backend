034d91d7bc7a86d2b4e5e24e7f503363d6d7df79
NeoRise456 <neorise456@gmail.com>
2024-11-16 03:05:17 -0500

    feat(iam): add query service for users.

---
+++ NRG3.Bliss.API/IAM/Domain/Services/IUserQueryService.cs
@@ -0,0 +1,16 @@
+﻿
+using NRG3.Bliss.API.IAM.Domain.Model.Aggregate;
+using NRG3.Bliss.API.IAM.Domain.Model.Queries;
+
+namespace NRG3.Bliss.API.IAM.Domain.Services;
+
+public interface IUserQueryService
+{
+    Task<User?> Handle(GetUserByIdQuery query);
+    
+    //TODO: Verify if this is correct (Alex)
+    /*
+     * lo que pasa es que si se quiere hacer un query que devuelva una lista de citas de un usuario, se debe de hacer en el contexto de appointments y no en el contexto de usuarios, porque estamos devolviendo ps appointments. xd
+     */
+    //Task<User?> Handle(GetUserAppointmentsById query);
+}
