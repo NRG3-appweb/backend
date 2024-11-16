81a42073e3dfe05b38ffcef71949fc69582a6b2b
NeoRise456 <neorise456@gmail.com>
2024-11-16 03:02:39 -0500

    feat(shared): added simplified service resource.

---
+++ NRG3.Bliss.API/Shared/Interfaces/REST/Resources/SimplifiedServiceResource.cs
@@ -0,0 +1,9 @@
+﻿namespace NRG3.Bliss.API.Shared.Interfaces.REST.Resources;
+
+public record SimplifiedServiceResource(
+    int id,
+    int companyId,
+    int categoryId,
+    string serviceName,
+    double price
+    );
