00771a24ffe114544fe7fadc9c2764fbd434ec9b
NeoRise456 <neorise456@gmail.com>
2024-11-16 03:21:06 -0500

    feat(appointment-management): add facade for appointment context.

---
+++ NRG3.Bliss.API/AppointmentManagement/Application/ACL/AppointmentContextFacade.cs
@@ -0,0 +1,21 @@
+﻿using NRG3.Bliss.API.AppointmentManagement.Domain.Model.Aggregates;
+using NRG3.Bliss.API.AppointmentManagement.Domain.Model.Queries;
+using NRG3.Bliss.API.AppointmentManagement.Domain.Services;
+using NRG3.Bliss.API.AppointmentManagement.Interfaces.ACL;
+
+namespace NRG3.Bliss.API.AppointmentManagement.Application.ACL.Services;
+
+public class AppointmentContextFacade(
+    IAppointmentCommandService appointmentCommandService,
+    IAppointmentQueryService appointmentQueryService
+    ) : IAppointmentContextFacade
+{
+    public async Task<IEnumerable<Appointment?>> FetchAppointmentsByUserIdAsync(int userId)
+    {
+        var getAppointmentsByUserIdQuery = new GetAppointmentsByUserIdQuery(userId);
+        
+        var appointments = await appointmentQueryService.Handle(getAppointmentsByUserIdQuery);
+        
+        return appointments;
+    }
+}
