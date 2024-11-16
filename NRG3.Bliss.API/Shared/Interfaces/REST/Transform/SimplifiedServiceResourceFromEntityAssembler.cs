70220dc34ca4c4a5bf055b0f823aa49a21c26def
NeoRise456 <neorise456@gmail.com>
2024-11-16 03:02:51 -0500

    feat(shared): added simplified service assembler.

---
+++ NRG3.Bliss.API/Shared/Interfaces/REST/Transform/SimplifiedServiceResourceFromEntityAssembler.cs
@@ -0,0 +1,18 @@
+﻿using NRG3.Bliss.API.ServiceManagement.Domain.Model.Aggregates;
+using NRG3.Bliss.API.Shared.Interfaces.REST.Resources;
+
+namespace NRG3.Bliss.API.Shared.Interfaces.REST.Transform;
+
+public class SimplifiedServiceResourceFromEntityAssembler
+{
+    public static SimplifiedServiceResource ToResourceFromEntity(Service entity)
+    {
+        return new SimplifiedServiceResource(
+            entity.Id,
+            entity.CompanyId,
+            entity.CategoryId,
+            entity.ServiceName,
+            entity.Price
+        );
+    }
+}
