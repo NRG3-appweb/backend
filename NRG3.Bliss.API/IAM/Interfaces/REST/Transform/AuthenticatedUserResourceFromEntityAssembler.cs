d073281e91ba7266c13c74c8ebaeae129954c857
NeoRise456 <neorise456@gmail.com>
2024-11-20 07:50:42 -0500

    feat(iam): add resources and assemblers for sign in.

---
+++ NRG3.Bliss.API/IAM/Interfaces/REST/Transform/AuthenticatedUserResourceFromEntityAssembler.cs
@@ -0,0 +1,12 @@
+﻿using NRG3.Bliss.API.AppointmentManagement.Domain.Model.Entities;
+using NRG3.Bliss.API.IAM.Interfaces.REST.Resources;
+
+namespace NRG3.Bliss.API.IAM.Interfaces.REST.Transform;
+
+public class AuthenticatedUserResourceFromEntityAssembler
+{
+    public static AuthenticatedUserResource ToResourceFromEntity(User user, string token)
+    {
+        return new AuthenticatedUserResource(user.Id, user.Email, token);
+    }
+}
